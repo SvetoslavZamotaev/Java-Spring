2759c006ec2e097d921093ab2b31474c08a76135
Svetoslav Zamotaev <zamatory1990@gmail.com>
2024-02-12 17:42:38 +0300

    seminar 9 added

--- sem5_springdata_home_task/src/main/java/com/example/sem5_springdata_home_task/Sem5SpringdataHomeTaskApplication.java
+++ sem5_springdata_home_task/src/main/java/com/example/sem5_springdata_home_task/Sem5SpringdataHomeTaskApplication.java
@@ -2,6 +2,9 @@ package com.example.sem5_springdata_home_task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Sem5SpringdataHomeTaskApplication {
@@ -9,4 +12,11 @@ public class Sem5SpringdataHomeTaskApplication {
 		SpringApplication.run(Sem5SpringdataHomeTaskApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("mcrservice1",r->r.path("/serviceA/**")
+						.uri("http://localhost:8081/"))
+				.route("Microservice2",r->r.path("/serviceB/**")
+						.uri("http://localhost:8082/")).build();}
 }
